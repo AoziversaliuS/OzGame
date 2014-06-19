e951c22a7ea32ddf258534ecdf9d3fffe1bc35fb
AoziversaliuS <1220310838@qq.com>
2014-06-19 16:34:40 +0800

    准备制作暂停按钮

---
+++ src/g/button/PauseBttons.java
@@ -0,0 +1,53 @@
+package g.button;
+
+import g.refer.OzElement;
+import g.refer.Player;
+import g.tool.OzPoint;
+import g.tool.OzRect;
+import g.type.ET;
+
+public class PauseBttons extends OzElement{
+
+	public PauseBttons(String Tag, int Rank, ET type, OzPoint l,
+			OzRect entityOffset) {
+		super(Tag, Rank, type, l, entityOffset);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void impact(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean rollBack() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
