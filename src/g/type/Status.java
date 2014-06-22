799a7dc418467d1a6e126fda6a0c13939325967f
AoziversaliuS <1220310838@qq.com>
2014-06-22 13:53:40 +0800

    备份

--- src/g/type/Status.java
+++ src/g/type/Status.java
@@ -1,6 +1,12 @@
 package g.type;
 
 public enum Status {
-	Start,Select,Credits,Loading,Game,Pause,
+	Start,//主菜单
+	Select,//关卡菜单
+	Credits,//制作人员
+	Loading,//载入
+	Game,//游戏界面
+	Pause,//暂停界面
+	Interlude,//切换关卡的过场界面
 	Init//初始化状态
 }
